2fe96a2b6ca515bc20d09a44365b4c15d23be48a
Gonzalo Matheu (framework) <gmatheu@simtlix.com>
2023-07-18 12:39:43 -0300

    Adding bat download from release pages (only unknown linux x86_64)

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,8 +2,7 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for bat.
-GH_REPO="https://github.com/gmatheu/asdf-bat"
+GH_REPO="https://github.com/sharkdp/bat"
 TOOL_NAME="bat"
 TOOL_TEST="bat --version"
 
@@ -31,7 +30,6 @@ list_github_tags() {
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
 	# Change this function if bat has other means of determining installable versions.
 	list_github_tags
 }
@@ -41,8 +39,7 @@ download_release() {
 	version="$1"
 	filename="$2"
 
-	# TODO: Adapt the release URL convention for bat
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	url="$GH_REPO/releases/download/v${version}/bat-v${version}-x86_64-unknown-linux-gnu.tar.gz"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
